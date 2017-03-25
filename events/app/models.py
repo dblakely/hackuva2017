b6f06b93dffa445d75c8ebd97b53a4def8661504
Brandon Purvis <bcpmax@gmail.com>
2017-03-25 16:59:27 -0400

    Create Event model

--- events/app/models.py
+++ events/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+    Represents a single event.
+    """
+    name = models.CharField(max_length=300)
+    location_name = models.CharField(max_length=300)
+    datetime = models.DateTimeField()
+    latitude = models.DecimalField(max_digits=50, decimal_places=20)
+    longitude = models.DecimalField(max_digits=50, decimal_places=20)
+
